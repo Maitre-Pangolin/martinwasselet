f8762c0189ef134fcba480da5ebc90cd30c5089c
Maitre-Pangolin <martin.wasselet@gmail.com>
2021-10-12 11:45:41 -0400

    first commit

---
+++ sketch.js
@@ -0,0 +1,11 @@
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(0, 000, 200);
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
