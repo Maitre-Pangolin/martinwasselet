6dfdac695c11048cfb616319fa4375d9acf53f0f
Maitre-Pangolin <martin.wasselet@gmail.com>
2021-10-28 16:41:00 -0400

    Inside IIFE

--- effect.js
+++ effect.js
@@ -1,20 +1,22 @@
-const fullpage = document.querySelector("#fullpage");
-const section = document.querySelector("#info-section");
-const artwork = document.querySelector("#artwork");
-let lastScrollTop = window.scrollY;
+(function () {
+  const fullpage = document.querySelector("#fullpage");
+  const section = document.querySelector("#info-section");
+  const artwork = document.querySelector("#artwork");
+  let lastScrollTop = window.scrollY;
 
-window.addEventListener("scroll", () => {
-  console.log("scroll");
-});
+  window.addEventListener("scroll", () => {
+    console.log("scroll");
+  });
 
-fullpage.addEventListener(
-  "scroll",
-  _.throttle(({ target: { scrollTop } }) => scrollControl(scrollTop), 1000)
-);
+  fullpage.addEventListener(
+    "scroll",
+    _.throttle(({ target: { scrollTop } }) => scrollControl(scrollTop), 1000)
+  );
 
-const scrollControl = (scrollTop) => {
-  if (scrollTop < window.innerHeight && scrollTop > lastScrollTop) {
-    section.scrollIntoView();
-  }
-  lastScrollTop = scrollTop;
-};
+  const scrollControl = (scrollTop) => {
+    if (scrollTop < window.innerHeight && scrollTop > lastScrollTop) {
+      section.scrollIntoView();
+    }
+    lastScrollTop = scrollTop;
+  };
+})();
